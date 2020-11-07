724cbc56e5ffaa0838bc8f2d40b125cbbb9d59f6
EmilyLow <emlow@me.com>
2020-11-06 21:24:46 -0600

    Login tests done

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -1,1 +1,38 @@
 const request = require('supertest');
+
+const server = require("../api/server");
+const dbConfig = require('../database/dbConfig');
+
+beforeEach(async () => {
+    await dbConfig.seed.run()
+})
+
+afterAll(async () => {
+    await dbConfig.destroy();
+})
+describe('login', () => {
+    it("should return 401 if user does not exist", async () => {
+        const expectedStatusCode = 401;
+        console.log("Inside first test");
+        const res = await request(server)
+            .post("/api/auth/login")
+            .send({username: "fakeUser", password: "password"})
+        // console.log("res", res);
+        expect(res.statusCode).toBe(expectedStatusCode) 
+        // expect(1).toBe(1)
+
+    })
+    it("should return 200 if user logs in", async () => {
+        const expectedStatusCode = 200;
+        console.log("Inside second test");
+        const res = await request(server)
+            .post("/api/auth/login")
+            .send({username: "User4", password: "password"})
+        // console.log("res", res);
+        expect(res.statusCode).toBe(expectedStatusCode) 
+        // expect(1).toBe(1)
+         await request(server)
+            .get("/api/auth/logout")
+
+    })
+})
--- database/seeds/001-auth.js
+++ database/seeds/001-auth.js
@@ -1,13 +1,13 @@
+const bycrpt = require("bcryptjs");
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+
+
+exports.seed = async function(knex) {
+	await knex("users").truncate()
+	await knex("users").insert([
+		{ username: "User1", password:  await bycrpt.hashSync("password", 8) },
+		{ username: "User2", password: await bycrpt.hashSync("password", 8) },
+		{ username: "User3", password: await bycrpt.hashSync("password", 8) },
+		{ username: "User4", password: await bycrpt.hashSync("password", 8) },
+	])
+}
